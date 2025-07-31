44a9d3b5e592761f6af3283fdea907ac5cc54fff
sam-a-d <masamad.sust@gmail.com>
2025-07-31 14:51:40 +0300

    Dag updated

--- dags/dags/helloworldDAG.py
+++ dags/dags/helloworldDAG.py
@@ -1,22 +1,18 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
 from datetime import datetime
 
-def say_hello():
-    print("Hello, Airflow is working!")
-
-# Define the DAG
-with DAG(
-    dag_id='hello_world_dag',
+@dag(
     start_date=datetime(2023, 1, 1),
-    schedule_interval='@once',  # Run only once
-    catchup=False,              # Do not backfill missed runs
-    tags=['example'],
-) as dag:
+    schedule="@once",
+    catchup=False,
+    tags=["example"],
+)
+def hello_world_dag():
+    @task
+    def say_hello():
+        print("Hello, Airflow is working!")
 
-    hello_task = PythonOperator(
-        task_id='say_hello_task',
-        python_callable=say_hello,
-    )
+    say_hello()
 
-    hello_task  # This defines the task in the DAG
+dag = hello_world_dag()
+
