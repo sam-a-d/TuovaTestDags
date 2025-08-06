a636fbb99d823732b1dd369d743617daef2914a4
sam-a-d <masamad.sust@gmail.com>
2025-08-06 20:18:31 +0300

    Test DAG added

---
+++ dags/testDAG.py
@@ -0,0 +1,18 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+@dag(
+    start_date=datetime(2023, 1, 1),
+    schedule="@once",
+    catchup=False,
+    tags=["example"],
+)
+def test_dag():
+    @task
+    def say_hello():
+        print("The test is passed")
+
+    say_hello()
+
+dag = test_dag()
+
